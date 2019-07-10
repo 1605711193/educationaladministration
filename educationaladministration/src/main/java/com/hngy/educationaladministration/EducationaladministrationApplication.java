fa197b1e9810d6a373b941fcadc4486cc22eec05
zsq <1872153705@qq.com>
2019-07-10 18:44:26 +0800

    创建项目框架

---
+++ educationaladministration/src/main/java/com/hngy/educationaladministration/EducationaladministrationApplication.java
@@ -0,0 +1,13 @@
+package com.hngy.educationaladministration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationaladministrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EducationaladministrationApplication.class, args);
+    }
+
+}
